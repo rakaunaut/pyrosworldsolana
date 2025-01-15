0a30575c7134b79076f51c42c13280394bf7109c
rakaunaut <rakaunaut@mail.de>
2025-01-15 11:53:25 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
+import svelte from '@astrojs/svelte';
+
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), svelte()]
 });
---
+++ src/components/Test.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+
+    let number: number = 0;
+
+</script>
+
+<button class="bg-yellow-200 rounded-full p-2 text-bgDark1" on:click={() => number++} > New Number: {number} </button>
